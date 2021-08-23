a8c9392ce9592fc80eba8a8cfa0c090f3e2f6e91
Weret90 <apostol-ea@mail.ru>
2021-08-23 23:22:18 +0900

    Добавил BottomNavMenu

--- app/src/main/java/com/umbrella/mymovieskotlin/view/MainActivity.kt
+++ app/src/main/java/com/umbrella/mymovieskotlin/view/MainActivity.kt
@@ -1,30 +1,24 @@
 package com.umbrella.mymovieskotlin.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.umbrella.mymovieskotlin.R
+import com.umbrella.mymovieskotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.item_main) {
-            findNavController(R.id.fragmentContainerView).navigate(R.id.filmsFragment)
+        binding.bottomNavigationMenu.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_main -> findNavController(R.id.fragmentContainerView).navigate(R.id.filmsFragment)
+                R.id.item_favourite -> findNavController(R.id.fragmentContainerView).navigate(R.id.favouriteFragment)
+            }
+            true
         }
-        if (item.itemId == R.id.item_favourite) {
-            findNavController(R.id.fragmentContainerView).navigate(R.id.favouriteFragment)
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
